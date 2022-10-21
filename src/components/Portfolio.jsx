b5adad4e740cdd05d6c4e6aa000304ca68565916
Dakshathkumar N <dakshathkumar@gmail.com>
2022-10-21 15:15:08 +0530

    pproject

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -101,10 +101,10 @@ const Portfolio = () => {
               <div className="flex items-center justify-center">
             
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"  >
-                <a href={LiveDemo}>LiveDemo</a>
+                <a href={LiveDemo}>View</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                <a href={href}>  <FaGithub size={50} /></a>
+                <a href={href}>  <FaGithub size={25} /></a>
               
                 </button>
               </div>
